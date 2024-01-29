31c5ee748f28df7fc7b61f11a3a70ebaefdd22b3
Juan Ignacio Larraìn <ji.larrain@uc.cl>
2024-01-29 17:14:28 -0300

    New backup from 29-01-24

--- script.sh
+++ script.sh
@@ -31,4 +31,4 @@ git filter-branch --force --index-filter \
 #git rm -rf --cached "$parent_path"/.env
 git add "$parent_path"
 git commit -m "New backup from $(date +"%d-%m-%y")"
-git push https://"$github_token"@github.com/"$github_username"/"$github_repository".git
+git push --force https://"$github_token"@github.com/"$github_username"/"$github_repository".git
